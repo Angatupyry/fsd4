c0c46eeecc98f594092a2bfe06a0f6776c9248c4
Angatupyry <fierrofenix@gmail.com>
2021-09-30 17:15:48 -0400

    Borar tarea

--- Javascript/Clase10/script.js
+++ Javascript/Clase10/script.js
@@ -1,10 +1,12 @@
 const enterButton = document.getElementById("enter");
 const input = document.getElementById("userInput");
 const ul = document.querySelector("ul");
+const item = document.getElementsByTagName("li");
 
 function createListElement() {
   const li = document.createElement("li");
   li.textContent = input.value;
+  //li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
   input.value = "";
 
@@ -13,6 +15,15 @@ function createListElement() {
   }
 
   li.addEventListener("click", tachar);
+
+  const dBtn = document.createElement("button");
+  dBtn.appendChild(document.createTextNode("X"));
+  li.appendChild(dBtn);
+  dBtn.addEventListener("click", deleteListItem);
+
+  function deleteListItem() {
+    li.classList.add("delete");
+  }
 }
 
 function addTarea() {
